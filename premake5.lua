0cd36110623d8c6845421161b64138baa9cc63e9
xltangcs <xltangcs@outlook.com>
2026-06-27 04:45:43 +0800

    Add premake5.lua for CandyEngine build

---
+++ premake5.lua
@@ -0,0 +1,38 @@
+project "Imgui"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_tables.cpp",
+		"imgui_widgets.cpp",
+		"*.h"
+	}
+
+	includedirs
+	{
+		"."
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
